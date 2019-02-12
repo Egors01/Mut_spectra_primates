53943554091f3ba1d33920bf50981a1b912c2be3
Egors01 <Egor.Semenchenko@Skoltech.ru>
2019-02-12 15:51:25 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Mut_spectra_primates',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Egors01/Mut_spectra_primates',
+    license='',
+    author='egors',
+    author_email='',
+    description=''
+)
